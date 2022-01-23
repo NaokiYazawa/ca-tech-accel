ac6b0c1cc6cf43ce2ac65bb22202cb3c09f100b8
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-01-23 21:19:25 +0900

    fix w.Write([]byte("pong")) in ping.go

--- handler/ping.go
+++ handler/ping.go
@@ -7,12 +7,11 @@ import (
 func Ping() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO: pongを返してみる
-		if _, err := w.Write([]byte("")); err != nil {
+		if _, err := w.Write([]byte("pong")); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		w.Write([]byte("pong"))
 		w.WriteHeader(http.StatusOK)
 	}
 }
